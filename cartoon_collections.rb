77d74e25cfbc82735f2e24211a664cfc46ab1045
Courtney Dyson <courtneydyson87@gmail.com>
2019-07-07 23:46:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves = %w[Doc Dopey Bashful Grumpy].each_with_index {|item, index|}
+  puts dwarves
 end
 
 def summon_captain_planet# code an argument here
