c8db11f14e068831df3359fe995cdcf9c3e50470
Courtney Dyson <courtneydyson87@gmail.com>
2019-07-08 00:18:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
   i = 0
-  while i < array.length
+  while i < dwarves.length
     puts "#{i + 1}. #{dwarves[i]}"
     i += 1
   end
